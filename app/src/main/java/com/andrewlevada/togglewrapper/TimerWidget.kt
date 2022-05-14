546d3903d02c422f7783ff422728a0469aed9b06
AndrewLevada <levada.andrew@gmail.com>
2022-05-14 13:26:46 +0300

    feature(): created basic layout with styling

--- app/src/main/java/com/andrewlevada/togglewrapper/TimerWidget.kt
+++ app/src/main/java/com/andrewlevada/togglewrapper/TimerWidget.kt
@@ -34,10 +34,8 @@ internal fun updateAppWidget(
     appWidgetManager: AppWidgetManager,
     appWidgetId: Int
 ) {
-    val widgetText = context.getString(R.string.appwidget_text)
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.timer_widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
